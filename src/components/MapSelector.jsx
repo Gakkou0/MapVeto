08f42b564e25459f9f52be2482ca94fa90c69b61
gakkou0 <fhugo.625@gmail.com>
2023-06-13 14:45:46 -0300

    first commit

---
+++ src/components/MapSelector.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function MapSelector(){
+    return (
+        <div className="MapBox">
+            <div className="MapVeto">
+                <div className="">
+                    <p>{`${teamOne}
+                        VETO MAP`}</p>
+                </div>
+                <div>
+                    <img></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MapSelector
