3787bb8551be781fe6cdcbd39f5e4cc859abddc8
gakkou0 <fhugo.625@gmail.com>
2023-11-28 19:04:39 -0300

    Selection animation and team's logo

--- src/components/MapSelector.jsx
+++ src/components/MapSelector.jsx
@@ -1,68 +1,89 @@
 import React from "react"
 import { useState } from 'react'
 import './MapSelector.css'
+import teamOneLogo from '../assets/teamOneLogo.png'
+import teamTwoLogo from '../assets/teamTwoLogo.png'
+import valorantLogo from '../assets/valorantLogo.png'
 
 function MapSelector(){
-    const teamOne = "ViperCore"
-    const teamTwo = "OsCaraVelho"
+    const teamOne = "FURIA"
+    const teamTwo = "LOUD"
+
+    const [classe, setClasse] = useState('MapSelect')
+
+    const changeState = () => {
+        (classe == 'MapSelect') ? setClasse('MapSelectOn') : setClasse('MapSelect')
+    }
+
+    let MapSelection = 0
 
     return (
         <div className="MapBox">
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>{teamOne}</p>
                     <p>VETO MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamOneLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className="{classe}">
                 <div className="">
                     <p>{teamTwo}</p>
                     <p>VETO MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamTwoLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>{teamOne}</p>
                     <p>SELECT MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamOneLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>{teamTwo}</p>
                     <p>SELECT MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamTwoLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>{teamOne}</p>
                     <p>VETO MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamOneLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>{teamTwo}</p>
                     <p>VETO MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={teamTwoLogo}></img>
                 </div>
             </div>
-            <div className="MapSelect">
+            <div className={classe}>
                 <div className="">
                     <p>DECIDER</p>
                     <p>MAP</p>
                 </div>
                 <div className="MapImage">
+                    <img id="teamImage" src={valorantLogo}></img>
                 </div>
+            </div>
+            <div className="mapButtons">
+                <button onClick={changeState}></button>
             </div>
         </div>
     )
